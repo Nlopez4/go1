19e7c3021bf085024354671953b854f5ae2d2e15
nataliel2514 <natalielopez2514@gmail.com>
2023-02-09 20:00:42 -0600

    build server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,34 @@
-package main
+package main 
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var tpl = template.Must(template.ParseFiles(("index.html")))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
+
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port,mux)
 }
